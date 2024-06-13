2d36eb6e066e048e6d2218f9b35a4543e7fd391b
Yudai Yokose <yudaiyokose@omba2024.local>
2024-06-14 04:59:33 +0900

    画像の投稿機能を追加！

--- app/models/post.rb
+++ app/models/post.rb
@@ -2,6 +2,7 @@ class Post < ApplicationRecord
   validates :content, {presence: true}
   validates :content, {length: {maximum: 140}}
   validates :user_id, {presence: true}
+  validates :image_name, {presence: true}
 
   def user
     return User.find_by(id: self.user_id)
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2024_06_13_064747) do
+ActiveRecord::Schema[7.0].define(version: 2024_06_13_083543) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -26,6 +26,7 @@ ActiveRecord::Schema[7.0].define(version: 2024_06_13_064747) do
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
     t.integer "user_id"
+    t.string "image_name"
   end
 
   create_table "users", force: :cascade do |t|
