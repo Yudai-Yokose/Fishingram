23b9b3c8fe77b71a82e3760f9ebb77b0d18b11e6
Yudai Yokose <yudaiyokose@omba2024.local>
2024-08-25 04:44:30 +0900

    set devise gem

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -99,4 +99,15 @@ Rails.application.configure do
   # ]
   # Skip DNS rebinding protection for the default health check endpoint.
   # config.host_authorization = { exclude: ->(request) { request.path == "/up" } }
+  config.action_mailer.default_url_options = { host: 'https://fishingram-3dead5b2b24d.herokuapp.com/' }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address:              'smtp.gmail.com',
+    port:                 587,
+    domain:               'fishingram-3dead5b2b24d.herokuapp.com',
+    user_name:            Rails.application.credentials.mailer[:sender],
+    password:             Rails.application.credentials.mailer[:password],
+    authentication:       'plain',
+    enable_starttls_auto: true
+  }
 end
