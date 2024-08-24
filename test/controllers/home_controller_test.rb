57c7a4d155d7e486b82c9d88080871cd0c00f019
Yudai Yokose <yudaiyokose@omba2024.local>
2024-08-25 05:51:59 +0900

    prepare_test

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,6 +2,7 @@ require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   setup do
+    User.delete_all
     @user = User.create!(
       email: "user_#{SecureRandom.hex(10)}@example.com",
       password: "password"
