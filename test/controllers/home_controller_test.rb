12486d1575d77079f8fc8c72e621cf67d17a5ace
Yudai Yokose <yudaiyokose@omba2024.local>
2024-08-25 05:47:53 +0900

    fix_home_controller_test

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -1,6 +1,13 @@
 require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @user = User.create!(
+      email: "user_#{SecureRandom.hex(10)}@example.com",
+      password: "password"
+    )
+  end
+
   test "should get index" do
     get root_url
     assert_response :success
