a87b044c73e2eb224b63cff3f5b2d0404e23b738
Yudai Yokose <yudaiyokose@omba2024.local>
2024-08-30 11:56:16 +0900

    fix_home_controller_helper

--- test/controllers/home_controller_test.rb
+++ test/controllers/home_controller_test.rb
@@ -2,7 +2,19 @@ require "test_helper"
 
 class HomeControllerTest < ActionDispatch::IntegrationTest
   setup do
-    User.delete_all
+    @user = User.create!(
+      username: "testuser",
+      email: "user_#{SecureRandom.hex(10)}@example.com",
+      password: "password"
+    )
+    unless @user.profile_image.attached?
+      @user.profile_image.attach(io: File.open(Rails.root.join("public/default_profile_image.png")), filename: "default_profile_image.png", content_type: "image/png")
+    end
+    sign_in @user
+  end
+  
+  setup do
+    User.destroy_all  # ここで delete_all ではなく destroy_all を使用する
     @user = User.create!(
       username: "testuser",
       email: "user_#{SecureRandom.hex(10)}@example.com",
