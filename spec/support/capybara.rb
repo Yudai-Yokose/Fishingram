4f931e206fb97e3e56bd0c54f84e0f4bb2cc9259
Yudai Yokose <yudaiyokose@omba2024.local>
2024-10-05 19:10:55 +0900

    rspec_12

--- spec/support/capybara.rb
+++ spec/support/capybara.rb
@@ -1,15 +1,14 @@
 require 'capybara/rspec'
-require 'selenium-webdriver'
 
-Capybara.register_driver :selenium_chrome_headless do |app|
-  url = ENV["SELENIUM_DRIVER_URL"]
-  capabilities = ::Selenium::WebDriver::Chrome::Options.new(
-    "goog:chromeOptions" => {
-      "args" => [
-        "no-sandbox",
-        "headless",
-        "disable-gpu",
-        "window-size=1680,1050"
+Capybara.register_driver :remote_chrome do |app|
+  url = ENV['SELENIUM_DRIVER_URL']
+  capabilities = ::Selenium::WebDriver::Remote::Capabilities.chrome(
+    'goog:chromeOptions' => {
+      'args' => [
+        'no-sandbox',
+        'headless',
+        'disable-gpu',
+        'window-size=1680,1050'
       ]
     }
   )
@@ -21,19 +20,18 @@ Capybara.register_driver :selenium_chrome_headless do |app|
 end
 
 RSpec.configure do |config|
-  config.include FactoryBot::Syntax::Methods
-  config.include Devise::Test::IntegrationHelpers, type: :system
   config.before(:each, type: :system) do
     driven_by :rack_test
   end
 
   config.before(:each, type: :system, js: true) do
-    Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
-    Capybara.server_port = 4444
-    Capybara.app_host = "http://#{Capybara.server_host}:#{Capybara.server_port}"
-    driven_by :selenium_chrome_headless
-  end
-  config.after(:each, type: :system) do
-    Capybara.reset_sessions!
+    if ENV['SELENIUM_DRIVER_URL']
+      Capybara.server_host = IPSocket.getaddress(Socket.gethostname)
+      Capybara.server_port = 4444
+      Capybara.app_host = "http://#{Capybara.server_host}:#{Capybara.server_port}"
+      driven_by :remote_chrome
+    else
+      driven_by :selenium_chrome_headless
+    end
   end
 end
